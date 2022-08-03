f97b64b267387473f9f21bc6ff06e202cf7e4a51
Jonathas Leontino Medina <emaildojonathas@gmail.com>
2022-08-03 10:45:22 -0400

    -

--- app/src/main/java/com/example/aulas178/MainActivity.java
+++ app/src/main/java/com/example/aulas178/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.aulas178;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -31,11 +33,29 @@ public class MainActivity extends AppCompatActivity {
                 String email = edEmail.getText().toString();
                 String senha = edSenha.getText().toString();
 
+                if(email.trim().equals("")||
+                        !Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    edEmail.setError("Preencha.");
+                    edEmail.requestFocus();
+                }
+                if(senha.trim().equals("")){
+                    edSenha.setError("Preencha.");
+                    edSenha.requestFocus();
+                }
+
+                Intent i = new Intent(
+                        MainActivity.this,
+                        MainActivity2.class
+                );
+
+                startActivity(i);
+
+
+
                 Toast.makeText(getApplicationContext(),
                         "O e-mail digitado foi: " + email +
                                 " e a senha digitada foi: " + senha,
                         Toast.LENGTH_LONG).show();
-
 
             }
         });
