3cbeb201419a14e214190fcceb099da650abb1b1
Jonathas Leontino Medina <emaildojonathas@gmail.com>
2022-08-09 08:34:22 -0400

    -

--- app/src/main/java/com/example/aulas178/MainActivity2.java
+++ app/src/main/java/com/example/aulas178/MainActivity2.java
@@ -11,7 +11,9 @@ import java.util.ArrayList;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    //campo de busca
     SearchView searchView;
+    //lista de itens
     ListView listView;
 
     ArrayList<String> arrayListItens = new ArrayList<>();
@@ -26,7 +28,6 @@ public class MainActivity2 extends AppCompatActivity {
         searchView  = findViewById(R.id.searchView1);
         listView  = findViewById(R.id.listView1);
 
-
         arrayListItens.add("Item 1");
         arrayListItens.add("Item 2");
         arrayListItens.add("Item 3");
@@ -35,32 +36,41 @@ public class MainActivity2 extends AppCompatActivity {
 
         arrayListItensCopia = new ArrayList<>(arrayListItens);
 
+        //adapter: vincular arrayList com a ListView
         meuAdapter = new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1,
                 arrayListItens
         );
 
+        //...usando o adapter. Aqui a lista é atualizada/exibida
         listView.setAdapter(meuAdapter);
 
+        //expande item
+        searchView.setIconified(false);
+        //fecha o teclado
+        searchView.clearFocus();
+
+        //ouvinte do searchView para mudanças na digitação
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            //este método é chamado no submit
             @Override
             public boolean onQueryTextSubmit(String s) {
                 return false;
             }
 
+            //este método é chamado ao digitar
             @Override
             public boolean onQueryTextChange(String s) {
 
+                //método para atualizar itens do arraylist
                 fazerBusca(s);
+                //atualiza lista com arrayList alterado
                 meuAdapter.notifyDataSetChanged();
-
 
                 return false;
             }
         });
-
-
     }
 
     private void fazerBusca(String s) {
@@ -69,11 +79,12 @@ public class MainActivity2 extends AppCompatActivity {
 
         s = s.toLowerCase();
 
+        //percorre array de cópia com todos os itens e busca
         for(String item: arrayListItensCopia){
             if(item.toLowerCase().contains(s)){
+                //se item encontrado, adicionada de volta no array
                 arrayListItens.add(item);
             }
         }
-
     }
 }
