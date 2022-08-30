67eb5d8fe3d43711334002d7271ddf222b2a5885
Jonathas Leontino Medina <emaildojonathas@gmail.com>
2022-08-30 08:40:34 -0400

    --

--- app/src/main/java/com/example/aulas178/MainActivityButtons.java
+++ app/src/main/java/com/example/aulas178/MainActivityButtons.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -13,6 +14,7 @@ public class MainActivityButtons extends AppCompatActivity {
 
     ImageButton imageButton_;
     FloatingActionButton floatingActionButton_;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,21 @@ public class MainActivityButtons extends AppCompatActivity {
 
         imageButton_ = findViewById(R.id.imageButton);
         floatingActionButton_ = findViewById(R.id.floatingActionButton);
+        textView = findViewById(R.id.textView2);
+
+        textView.setText(" aklsdjhfaksjd hfalkd jshflak jshdf lkajshfd lkajhsdf \" +\n" +
+                "                \"lkjahsd flkjahsd flkjha sdlkfjh alskjdhf lakjsdhf lakjsdhf \" +\n" +
+                "                \"lkajshdf asd f  sdaklsdjhfaksjd hfalkd jshflak jshdf lkajshfd lkajhsdf \" +\n" +
+                "                \"lkjahsd flkjahsd flkjha sdlkfjh alskjdhf lakjsdhf lakjsdhf \" +\n" +
+                "                \"lkajshdf asd f  sdaklsdjhfaksjd hfalkd jshflak jshdf lkajshfd lkajhsdf \" +\n" +
+                "                \"lkjahsd flkjahsd flkjha sdlkfjh alskjdhf lakjsdhf lakjsdhf \" +\n" +
+                "                \"lkajshdf asd f  sdaklsdjhfaksjd hfalkd jshflak jshdf lkajshfd lkajhsdf \" +\n" +
+                "                \"lkjahsd flkjahsd flkjha sdlkfjh alskjdhf lakjsdhf lakjsdhf \" +\n" +
+                "                \"lkajshdf asd f  sdaklsdjhfaksjd hfalkd jshflak jshdf lkajshfd lkajhsdf \" +\n" +
+                "                \"lkjahsd flkjahsd flkjha sdlkfjh alskjdhf lakjsdhf lakjsdhf \" +\n" +
+                "                \"lkajshdf asd f  sdaklsdjhfaksjd hfalkd jshflak jshdf lkajshfd lkajhsdf \" +\n" +
+                "                \"lkjahsd flkjahsd flkjha sdlkfjh alskjdhf lakjsdhf lakjsdhf \" +\n" +
+                "                \"lkajshdf asd f  sdfkj hsdkf jhs kdjfh sdf sdf sd fsd fsd fs df sdf sdf sfd sdf sdfs skjfdh skjdfh ksjd hfksjh d");
 
         floatingActionButton_.setOnClickListener(new View.OnClickListener() {
             @Override
